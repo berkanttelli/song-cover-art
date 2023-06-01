2941c15bba3386c90d845f4b84676e0660ae4919
Berkant <telliberkant@gmail.com>
2023-06-01 19:36:18 +0300

    git init

---
+++ src/main/java/com/dir/music/songcoverart/SongCoverArtApplication.java
@@ -0,0 +1,13 @@
+package com.dir.music.songcoverart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongCoverArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongCoverArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dir/music/songcoverart/SongCoverArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dir.music.songcoverart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongCoverArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

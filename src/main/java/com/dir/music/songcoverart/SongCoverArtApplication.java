f052b12c532b6c2d43c8a7d80e0bb423c447ade4
Berkant <telliberkant@gmail.com>
2023-06-03 17:49:35 +0300

    fix: annotation

--- src/main/java/com/dir/music/songcoverart/SongCoverArtApplication.java
+++ src/main/java/com/dir/music/songcoverart/SongCoverArtApplication.java
@@ -2,9 +2,10 @@ package com.dir.music.songcoverart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class SongCoverArtApplication {
 
     public static void main(String[] args) {
